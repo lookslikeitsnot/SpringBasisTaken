db9ac9f33b7f37cd92e4893d3b4c0e49dfc57ec0
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-03-15 09:31:06 +0100

    met enum persoonEigenschappen

--- src/main/java/be/vdab/PersoonViewer.java
+++ src/main/java/be/vdab/PersoonViewer.java
@@ -1,5 +1,6 @@
 package be.vdab;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class PersoonViewer {
@@ -11,10 +12,14 @@ public class PersoonViewer {
 
 	public void afbeelden(List<Persoon> personen) {
 		for (Persoon persoon : personen) {
-			System.out.println(persoon.getPersoonNr());
-			System.out.println(persoon.getVoornaam());
-			System.out.println(persoon.getFamilienaam());
-			System.out.println(persoon.getAantalKinderen());
+			if (Arrays.asList(eigenschappen).contains(PersoonEigenschap.PERSOON_NR))
+				System.out.println(persoon.getPersoonNr());
+			if (Arrays.asList(eigenschappen).contains(PersoonEigenschap.VOORNAAM))
+				System.out.println(persoon.getVoornaam());
+			if (Arrays.asList(eigenschappen).contains(PersoonEigenschap.FAMILIENAAM))
+				System.out.println(persoon.getFamilienaam());
+			if (Arrays.asList(eigenschappen).contains(PersoonEigenschap.AANTAL_KINDEREN))
+				System.out.println(persoon.getAantalKinderen());
 		}
 	}
 }
